8afe22b55bf42f77817337bb7ba8271b4557c969
Przemek <pwalczykk@gmail.com>
2016-07-12 11:27:16 +0200

    IP monitoring camera driver added - not tested

---
+++ rover_vision/src/ip_camera_driver/Main.cpp
@@ -0,0 +1,28 @@
+#include "CameraHandler.hpp"
+#include "ImagePublisher.hpp"
+
+int main(int argc, char** argv){
+
+    std::string stream_addres = argv[argc-2];
+    std::string publisher_topic = argv[argc-1];
+
+    ros::init(argc, argv, "ip_camera_driver");
+    ros::NodeHandle nh;
+    ros::Rate loop_rate(10);
+
+    ImagePublisher cam1(stream_addres, publisher_topic);
+
+    cam1.CameraHandler::Init();
+
+    // cam1.CameraHandler::Set(320, 240, 30);
+
+    while(ros::ok()){
+        cam1.CameraHandler::Init();
+        cam1.PublishImage();
+        loop_rate.sleep();
+    }
+
+    cam1.CameraHandler::Release();
+
+    return 0;
+}
