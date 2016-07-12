bf90cd1a3eb7378e38b1e530397dcc1b3a365779
Przemek <pwalczykk@gmail.com>
2016-07-12 14:35:45 +0200

    reciver and transmiter examples

---
+++ rover_rpi_uart/src/example/Reciver.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    // Open UART device
+    int uart0_filestream = -1;
+    uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if(uart0_filestream == -1){
+        printf("ERROR - Unable to acces UART\n");
+    }
+
+    // UART settings
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR;
+    options.c_iflag = 0;
+    options.c_iflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    if (uart0_filestream != -1)
+    {
+        // Read up to 255 characters from the port if they are there
+        unsigned char rx_buffer[256];
+        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+        if (rx_length < 0)
+        {
+            printf("'UART RX error'\n");
+        }
+        else if (rx_length == 0)
+        {
+            printf("No data");
+        }
+        else
+        {
+            // rx_buffer[rx_length] = '\0';
+            printf("%i bytes read : %s\n", rx_length, rx_buffer);
+        }
+    }
+
+    close(uart0_filestream);
+
+    return 0;
+}
--- rover_rpi_uart/src/example/Transmiter.cpp
+++ rover_rpi_uart/src/example/Transmiter.cpp
@@ -25,30 +25,20 @@ int main(int argc, char** argv){
     tcflush(uart0_filestream, TCIFLUSH);
     tcsetattr(uart0_filestream, TCSANOW, &options);
 
-    // Transmiting bytes
-    // unsigned char tx_buffer[BUFF_SIZE];
-    // unsigned char *p_tx_buffer;
-    //
-    // p_tx_buffer = &tx_buffer[0];
-    // *p_tx_buffer++ = 'H';
-    // *p_tx_buffer++ = 'e';
-    // *p_tx_buffer++ = 'l';
-    // *p_tx_buffer++ = 'l';
-    // *p_tx_buffer++ = 'o';
-    // *p_tx_buffer++ = '!';
-
     // Transmiting buffer
     int word_length = strlen(argv[argc-1]);
-
     char* tx_buffer = (char*)malloc(word_length * sizeof(unsigned char));
-
     tx_buffer = argv[argc-1];
 
+    // Transmiting bytes
     if (uart0_filestream != -1){
-        // write_function(Filestream, Begin of bytes to write, number of bytes to write);
         int count = write(uart0_filestream, (const void*)tx_buffer, word_length);
         if(count < 0){
             printf("'UART TX error'\n");
         }
     }
+
+    // Closing UART
+    close(uart0_filestream);
+    return 0;
 }
