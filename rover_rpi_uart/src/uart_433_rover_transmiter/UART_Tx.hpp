1a8f1f8c047df85e6c1a40f4b3654e923a5a6811
Przemek <pwalczykk@gmail.com>
2016-07-20 22:50:44 +0200

    removed tx_buffer

--- rover_rpi_uart/src/uart_433_rover_transmiter/UART_Tx.hpp
+++ rover_rpi_uart/src/uart_433_rover_transmiter/UART_Tx.hpp
@@ -16,7 +16,7 @@ class UART_Tx{
     int uart0_filestream;
     struct termios options;
 
-    int32_t* tx_buffer;
+    // int32_t* tx_buffer;
 public:
     UART_Tx(const char* device_addres){
         // Open UART device
@@ -36,21 +36,21 @@ public:
         tcsetattr(uart0_filestream, TCSANOW, &options);
 
         // Memory allocation for transmiting buffer
-        tx_buffer = (int32_t*)malloc(BUFF_SIZE * sizeof(int32_t));
+        // tx_buffer = (int32_t*)malloc(BUFF_SIZE * sizeof(int32_t));
     }
 
     ~UART_Tx(){
         close(uart0_filestream);
-        free(tx_buffer);
+        // free(tx_buffer);
     }
 
     void Transmit(int32_t* word){
         // Filling buffer
-        tx_buffer = word;
+        // tx_buffer = word;
 
         // Transmiting bytes
         if (uart0_filestream != -1){
-            int count = write(uart0_filestream, (const void*)tx_buffer, BUFF_SIZE * sizeof(int32_t));
+            int count = write(uart0_filestream, (const void*)word, BUFF_SIZE * sizeof(int32_t));
             if(count < 0){
                 printf("'UART TX error code: %d'\n", count);
             }
