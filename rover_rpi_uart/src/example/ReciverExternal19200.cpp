82eb4201872da493ff33e8178c09be6046699e87
Przemek <pwalczykk@gmail.com>
2016-08-08 15:28:42 +0200

    19200 uart example nodes

---
+++ rover_rpi_uart/src/example/ReciverExternal19200.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    // Open UART device
+    printf("filestream opening\n");
+
+    int uart0_filestream = -1;
+    uart0_filestream = open("/dev/ttyACM0", O_RDONLY | O_NOCTTY);
+    //uart0_filestream = open("/dev/ttyACM0", O_RDONLY | O_NOCTTY | O_NDELAY);
+
+    if(uart0_filestream == -1){
+        printf("ERROR - Unable to acces UART\n");
+    }
+
+    printf("filestream opened: %d\n", uart0_filestream);
+
+    // UART settings
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B19200 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    // UART RX Buffer
+    unsigned char rx_buffer[256];
+
+    printf("starting loop\n");
+
+    // Reciving data
+    while(1){
+        if (uart0_filestream != -1)
+        {
+            // Read up to 255 characters from the port if they are there
+            int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+            if (rx_length < 0)
+            {
+                printf("'UART RX error'\n");
+            }
+            else if (rx_length == 0)
+            {
+                //printf("No data\n");
+            }
+            else
+            {
+                rx_buffer[rx_length] = '\0';
+                printf("%i bytes read : %s\n", rx_length, rx_buffer);
+            }
+        }
+    }
+
+    close(uart0_filestream);
+    return 0;
+}
---
+++ rover_rpi_uart/src/example/TransmiterExternal19200.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    // Open UART device
+    int uart0_filestream = -1;
+    uart0_filestream = open("/dev/ttyACM0", O_WRONLY | O_NOCTTY | O_NDELAY);
+    if(uart0_filestream == -1){
+        printf("ERROR - Unable to acces UART\n");
+    }
+
+    // UART settings
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B19200 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    // Transmiting buffer
+    int word_length = strlen(argv[argc-1]);
+    char* tx_buffer = (char*)malloc(word_length * sizeof(unsigned char));
+    tx_buffer = argv[argc-1];
+
+    // Transmiting bytes
+    if (uart0_filestream != -1){
+        int count = write(uart0_filestream, (const void*)tx_buffer, word_length);
+        if(count < 0){
+            printf("'UART TX error'\n");
+        }
+    }
+
+    // Closing UART
+    close(uart0_filestream);
+    return 0;
+}
